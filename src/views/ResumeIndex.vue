d0a6cc001952a8117e428f3f97cc1edcbbcf0518
Ripster47 <mpolinski12@gmail.com>
2019-03-07 11:25:48 -0600

    initial commit

---
+++ src/views/ResumeIndex.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="resume-index">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {
+      
+    };
+  },
+  created: function() {},
+  methods: {}
+};
+</script>
---
+++ src/views/ResumeShow.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="resume-show">
+    <h1>{{ student.first_name}} {{ student.last_name }}</h1>
+    <div>
+      <h6>{{ student.email }}</h6>
+    </div>
+    <p></p>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {
+            first_name: "",
+            last_name: "",
+            email: "",
+            phone_number: "",
+            bio: "",
+            linkedin_url: "",
+            twitter_handle: "",
+            website_url: "",
+            resume_url: "",
+            github_url: "",
+            photo: "" 
+            };
+  },
+  created: function() {
+    this.student = {
+                    first_name: "Herman",
+                    last_name: "Munster",
+                    email: "bossmunster@gmail.com",
+                    phone_number: "18003456789",
+                    bio: "I am a monster, who somehow still has a happy family",
+                    linkedin_url: "www.linkedinurl.com",
+                    twitter_handle: "BossMunster",
+                    website_url: "www.websiteurl.com",
+                    resume_url: "www.resumeurl.com",
+                    github_url: "www.githuburl.com",
+                    photo: "picture"
+                    }
+  },
+  methods: {}
+};
+</script>
