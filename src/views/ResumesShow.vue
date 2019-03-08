08305d383e54726778f14b8aa849937cb8444109
pnb90 <phuocbui90@gmail.com>
2019-03-08 15:27:36 -0600

    fixed show?

--- src/views/ResumesShow.vue
+++ src/views/ResumesShow.vue
@@ -8,46 +8,43 @@
       <h6 class="text-center">{{ student.github_url }}</h6>
     </div>
   </div>
-<!--     <div class="row">
-      <div class="left-column col-sm-4 pt-5">
-        <h2 class="text-right font-weight-bold ">Bio</h2>
-        <h2 class="pt-5"></h2>
-      </div>
-      <div class="col-sm-8 pt-5">
-        <h6 class = "text-left bio">{{student.short_bio}}</h6>
-      </div>
-    </div> -->
-  <!--   <div class="row">
-      <div class="left-column col-sm-4">
-     <h2 class="text-right font-weight-bold">Skills</h2>
-     <h2 class="pt-5"></h2>
+  <div class="row">
+    <div class="left-column col-sm-4 pt-5">
+      <h2 class="text-right font-weight-bold ">Bio</h2>
+      <h2 class="pt-5"></h2>
     </div>
-   <div class="col-sm-8">
+    <div class="col-sm-8 pt-5">
+    <h6 class = "text-left bio">{{student.short_bio}}</h6>
+    </div>
+  </div>
+  <div class="row">
+    <div class="left-column col-sm-4">
+      <h2 class="text-right font-weight-bold">Skills</h2>
+      <h2 class="pt-5"></h2>
+    </div>
+    <div class="col-sm-8">
       <h6 class = "text-left">{{student.skills.skill}}</h6>
     </div>
-   </div>
-   <div class="row">
-     <div class="left-column col-sm-4">
+  </div>
+  <div class="row">
+    <div class="left-column col-sm-4">
       <h2 class="text-right font-weight-bold">Experiences</h2>
       <h2 class="pt-5"></h2>
-   </div>
-     <div class="col-sm-8">
+    </div>
+    <div class="col-sm-8">
       <h6 class="text-left">{{student.experiences.start_date}}</h6>
       <h6 class="text-left">{{student.experiences.end_date}}</h6>
       <h6 class="text-left">{{student.experiences.job_title}}</h6>
       <h6 class="text-left">{{student.experiences.company_name}}</h6>
       <h6 class="text-left">{{student.experiences.details}}</h6>
       <div class="pt-5"></div>
-     </div>
-    </div> -->
-
-
-
-<!--   <div class="row">
+    </div>
+  </div>
+  <div class="row">
     <div class="left-column col-sm-4">
      <h2 class="text-right font-weight-bold">Educations</h2>
      <h2 class="pt-5"></h2>
-  </div>    -->  
+    </div>     
      <div class="col-sm-8">
       <h6 class="text-left">{{student.educations.start_date}}</h6>
       <h6 class="text-left">{{student.educations.end_date}}</h6>
@@ -56,20 +53,16 @@
       <h6 class="text-left">{{student.educations.details}}</h6>
       <div class="pb-5"></div>
      </div>
-   <!-- </div> -->
-
-  <!--  <div class="row">
-      <div class="left-column col-sm-4">
+  </div>
+  <div class="row">
+    <div class="left-column col-sm-4">
       <h2 class="text-right font-weight-bold">Capstone</h2>
     </div>
     <div class="col-sm-8">
       <h6 class="text-left">{{student.capstone.name}}</h6>
       <h6 class="text-left">{{student.capstone.description}}</h6>
       <h6 class="text-left">{{student.capstone.url}}</h6>
-     </div>
-    </div> -->
-    <!-- </div> -->
-   </div>
+    </div>
   </div>
  </div>
 </template>
